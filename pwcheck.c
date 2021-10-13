690c5a5ea54d9c3448f7fc7e8678fd4b6ccb7d7b
Ondřej Zobal <ondrej@ondrejzobal.xyz>
2021-10-13 21:35:59 +0200

    Everything except substrings works

--- pwcheck.c
+++ pwcheck.c
@@ -8,6 +8,10 @@
 #define VERSION "0.1"
 
 const int POSITIONAL_ARGS_TOTAL = 2;
+const int MAX_PASSWORD_LENGTH = 100;
+const char MIN_ALLOWED_CHAR = ' ';
+const char MAX_ALLOWED_CHAR = '~';
+const char UNIQUE_CHAR_ARRAY_LENGTH = MAX_ALLOWED_CHAR - MIN_ALLOWED_CHAR + 1;
 
 // Whit the values set to -1 it is easier to see whether the argument processing
 // function works as it should.
@@ -46,49 +50,68 @@ int isStrInt(char *str) {
   return 1;
 }
 
+int parseLevel(char str[]) {
+  int level = atoi(str);
+  if (level < 1 || level > 4) {
+    fprintf(stderr,
+            "The given level value (%d) has to be between 1 and 4! Using "
+            "1...\n",
+            level);
+    return 1;
+  }
+  return level;
+}
+
+int parseParam(char str[]) {
+  int param = atoi(str);
+  // Todo edge cases for level and param combinations.
+  if (param < 0) {
+    fprintf(stderr, "The given PARAM value (%d) is not positive! Using 1...\n",
+            param);
+    return 1;
+  }
+  return param;
+}
+
 // Function for processing arguments.
 int processArgs(int argc, char *argv[]) {
   int positionalArgTracker = 0;
   for (int i = 1; i < argc; i++) {
     // Processing flags
-    if (strComp(argv[i], "--stats")) {
-      stats = true;
+    if (argv[i][0] == '-') {
+      if (strComp(argv[i], "--stats")) {
+        stats = true;
+      } else if (strComp(argv[i], "-l")) {
+        // Acounting for the value that has to follow the flag
+        i++;
+        level = parseLevel(argv[i]);
+      } else if (strComp(argv[i], "-p")) {
+        printf("Chagabaga\n");
+        // Acounting for the value that has to follow the flag
+        i++;
+        param = parseParam(argv[i]);
+      }
+      continue;
     }
 
     // Processing positional arguments
-    else {
-      if (!isStrInt(argv[i])) {
-        fprintf(stderr, "A positive number was expected in place of \"%s\"!\n",
-                argv[i]);
-        return 2;
-      }
-
-      switch (positionalArgTracker) {
-      case 0:
-        level = atoi(argv[i]);
-        if (level < 1 || level > 4) {
-          fprintf(stderr,
-                  "The given level value (%d) has to be between 1 and 4! Using "
-                  "1...\n",
-                  level);
-        }
-        break;
-      case 1:
-        param = atoi(argv[i]);
-        if (param < 0) {
-          fprintf(stderr,
-                  "The given PARAM value (%d) is not positive! Using 1...\n",
-                  param);
-        }
-        break;
-      default:
-        fprintf(stderr, "Too many postional arguments found (%d)!\n",
-                positionalArgTracker);
-        break;
-      }
-      ++positionalArgTracker;
+    if (!isStrInt(argv[i])) {
+      fprintf(stderr, "A positive number was expected in place of \"%s\"!\n",
+              argv[i]);
+      return 2;
     }
+
+    switch (positionalArgTracker) {
+    case 0:
+      level = parseLevel(argv[i]);
+      break;
+    case 1:
+      param = parseParam(argv[i]);
+      break;
+    }
+    ++positionalArgTracker;
   }
+  /*
   if (positionalArgTracker == POSITIONAL_ARGS_TOTAL) {
     // Finsihed looping through all given arguments and no values are
     // missing.
@@ -99,25 +122,31 @@ int processArgs(int argc, char *argv[]) {
           argv[0]);
   fprintf(stderr, "Using default values, level: %d, param: %d\n", level, param);
   return 0;
+*/
+  return 0;
 }
 
-// TODO Likely doesn't even work.
+// TODO Mostlikely doesn't even work.
 int countRepSubstrings(char *str, int length, int size) {
   int repetitions = 0;
 
+  // This loops moves the whole testing offset.
   for (int i = 0; i < length - size; i++) {
     bool identical = true;
 
+    // This one will be iterating through the str
     for (int j = 0; j < length; j++) {
+      // And this one will be comparing the individual substrings to the rest of
+      // the strings.
       for (int k = 0; k < size; k++) {
-        if (i + j == k) {
+        if (i + k == j) {
           continue;
         }
 
-        if (str[i + j] != str[k]) {
-          printf("\n%d + %d = %d, %d\n", i, j, i + j, k);
+        if (str[i + k] != str[j]) {
+          fprintf(stderr, "\n%d + %d = %d, %d\n", i, k, i + k, j);
           identical = false;
-          break;
+          goto bruh;
         }
       }
     }
@@ -125,14 +154,15 @@ int countRepSubstrings(char *str, int length, int size) {
     if (identical) {
       repetitions++;
     }
+  bruh:;
   }
 
-  fprintf(stderr, "Chugnala %d\n", repetitions);
+  fprintf(stderr, "Substrings %d\n", repetitions);
   return repetitions;
 }
 
 int processPassword(char *pass, int level, int param, bool *uniqueChars,
-                    int *shortest, int *average) {
+                    int *shortest, int *averageSum, int *averageCount) {
   int length = 0;
   int upper = 0;
   int lower = 0;
@@ -145,6 +175,7 @@ int processPassword(char *pass, int level, int param, bool *uniqueChars,
 
   // Analysing the password.
   for (int i = 0; pass[i] != '\0' && pass[i] != '\n'; i++) {
+    uniqueChars[((int)pass[i]) - MIN_ALLOWED_CHAR] = true;
     if (pass[i] >= 'a' && pass[i] <= 'z') {
       lower++;
     } else if (pass[i] >= 'A' && pass[i] <= 'Z') {
@@ -175,6 +206,11 @@ int processPassword(char *pass, int level, int param, bool *uniqueChars,
     length++;
   }
 
+  *averageSum += length;
+  *averageCount += 1;
+  if (length < *shortest || *shortest == 0) {
+    *shortest = length;
+  }
 #ifdef DEBUG
   fprintf(stderr,
           "Length: %d, Upeer: %d, Lower: %d, Numbers: %d, Special: %d, "
@@ -182,31 +218,35 @@ int processPassword(char *pass, int level, int param, bool *uniqueChars,
           length, upper, lower, number, special, repSequence);
 #endif
 
-  if (length < 1) {
+  if (level >= 1 && (upper < 0 || lower < 0)) {
+    return 0;
+  }
+  if (level >= 2 &&
+      (upper < param || lower < param || number < param || special < param)) {
+    return 0;
+  }
+  if (level >= 3 && repSequence != 0) {
+    return 0;
+  }
+  if (level == 4 && countRepSubstrings(pass, length, param)) {
     return 0;
   }
 
-  if (level >= 1 && upper < 0 && lower < 0) {
-    printf("\nHelun1\n");
-    return 1;
-  }
-  if (level >= 2 && upper < param && lower < param && number < param &&
-      special > param) {
-    printf("\nHelun2\n");
-    return 1;
-  }
-  if (level >= 3 && repSequence != 0) {
-    printf("\nHelun3\n");
-    return 1;
-  }
-  if (level == 4 && countRepSubstrings(pass, length, param)) {
-    return 1;
-  }
-
-  return 0;
+  return 1;
 }
 
-void printStats() { return; }
+void printStats(bool uniques[], int shortest, int avgSum, int avgCount) {
+  int uniqueCount = 0;
+  for (int i = 0; i < UNIQUE_CHAR_ARRAY_LENGTH; i++) {
+    if (uniques[i]) {
+      uniqueCount++;
+    }
+  }
+
+  fprintf(stdout, "\nStatistics:\n");
+  fprintf(stdout, "Unique Symbols: %d\nShortest: %d\nAverage: %.2f\n",
+          uniqueCount, shortest, ((float)avgSum) / avgCount);
+}
 
 int main(int argc, char *argv[]) {
 #ifdef DEBUG
@@ -221,30 +261,43 @@ int main(int argc, char *argv[]) {
     return exitCode;
   }
 
-  // TODO Managing the stdin
-  char password[101];
-  int ch;
-  int counter;
-  while ((ch = getchar()) != EOF) {
-    printf("%c", ch); // TODO remove
-    password[counter] = ch;
-    counter++;
+  int statShortest = 0;
+  int statAverageSum = 0;
+  int statAverageCount = 0;
+  bool statUniqueChars[UNIQUE_CHAR_ARRAY_LENGTH];
+  // The array already contains data when it get's created and I am not sure
+  // why. It should be memory corruption, but there is nothing that could cause
+  // memoryc corruption that runs before it and also the rest of the memory
+  // seems fine as well
+  for (int i = 0; i < UNIQUE_CHAR_ARRAY_LENGTH; i++) {
+    statUniqueChars[i] = false;
   }
-
-  int statShortest = -1;
-  int statAverage = -1;
-  bool statUniqueChars['~' - ' ' + 1];
 
   // TODO Validating individual passwords.
   // for every password
-  if (!processPassword(password, level, param, statUniqueChars, &statShortest,
-                       &statAverage)) {
-    printf("Success: %s\n", password);
+  int counter = 0;
+  char ch;
+  while ((ch = getchar()) != EOF) {
+    char password[MAX_PASSWORD_LENGTH];
+    if (ch == '\n' || counter == MAX_PASSWORD_LENGTH) {
+      password[counter] = '\0';
+#ifdef DEBUG
+      fprintf(stderr, "Testing: %sEND\n", password);
+#endif
+      if (processPassword(password, level, param, statUniqueChars,
+                          &statShortest, &statAverageSum, &statAverageCount)) {
+        printf("%s\n", password);
+      }
+
+      counter = 0;
+      password[counter] = '\0';
+    } else {
+      password[counter] = ch;
+      counter++;
+    }
   }
 
-  /*
   if (stats) {
-    printStats();
+    printStats(statUniqueChars, statShortest, statAverageSum, statAverageCount);
   }
-*/
 }
