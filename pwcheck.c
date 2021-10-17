7a0ead8c656be3e87e7a3b29c4ca3e8835c39bcd
Ondřej Zobal <ondrej@ondrejzobal.xyz>
2021-10-17 21:56:35 +0200

    It almost works almost

--- pwcheck.c
+++ pwcheck.c
@@ -1,9 +1,14 @@
+/*
+** 1. IZP projekt.
+** @author Ondřej Zobal <xzobal01@stud.fit.vutbr.cz>
+*/
+
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 
 // Comment or uncomment the following line to toggle debugging.
-#define DEBUG 1
+//#define DEBUG 1
 #define AUTHOR "Ondřej Zobal"
 #define VERSION "0.1"
 
@@ -29,7 +34,12 @@ int param = 1;
 
 bool stats = false;
 
-// A function for comparing two char arrays.
+/*
+** A function for comparing strings.
+** @param first first string to be compared.
+** @param second second string to be compared.
+** @return 1 if strings are identical, 0 if thay differ.
+*/
 int strComp(char *first, char *second) {
   for (int i = 0; 1; i++) {
     if (first[i] != second[i]) {
@@ -40,7 +50,11 @@ int strComp(char *first, char *second) {
   }
 }
 
-// This function will return 1 if a given char array is an int.
+/*
+** Tests if a string is a valid integer.
+** @param str the string that will be tested
+** @return 1 if the string is a number, 0 if it isn't.
+*/
 int isStrInt(char *str) {
   for (int i = 0; str[i] != '\0'; i++) {
     if (str[i] < '0' || str[i] > '9') {
@@ -50,6 +64,11 @@ int isStrInt(char *str) {
   return 1;
 }
 
+/*
+** Parses a string as a 'level' value.
+** @param str the string to be tested.
+** @return The numeric value of the parsed str.
+*/
 int parseLevel(char str[]) {
   int level = atoi(str);
   if (level < 1 || level > 4) {
@@ -57,23 +76,34 @@ int parseLevel(char str[]) {
             "The given level value (%d) has to be between 1 and 4! Using "
             "1...\n",
             level);
-    return 1;
+    // return -1;
   }
   return level;
 }
 
+/*
+** Parses a string as a 'param' value.
+** @param str the string to be tested.
+** @return The numeric value of the parsed str.
+*/
 int parseParam(char str[]) {
   int param = atoi(str);
   // Todo edge cases for level and param combinations.
-  if (param < 0) {
+  if (param < 1) {
     fprintf(stderr, "The given PARAM value (%d) is not positive! Using 1...\n",
             param);
-    return 1;
+    // return -1;
   }
   return param;
 }
 
-// Function for processing arguments.
+/*
+** A function that sets the program's variables according to the users
+*arguments.
+** @param argc Number of indexes in the argv array.
+** @param argv array of command line argumets.
+** @return an error code.
+*/
 int processArgs(int argc, char *argv[]) {
   int positionalArgTracker = 0;
   for (int i = 1; i < argc; i++) {
@@ -83,13 +113,23 @@ int processArgs(int argc, char *argv[]) {
         stats = true;
       } else if (strComp(argv[i], "-l")) {
         // Acounting for the value that has to follow the flag
-        i++;
-        level = parseLevel(argv[i]);
+        if (++i >= argc) {
+          return 1;
+        }
+        if ((level = parseLevel(argv[i])) == -1) {
+          return 1;
+        }
       } else if (strComp(argv[i], "-p")) {
-        printf("Chagabaga\n");
         // Acounting for the value that has to follow the flag
-        i++;
-        param = parseParam(argv[i]);
+        if (++i >= argc) {
+          return 1;
+        }
+        if ((param = parseParam(argv[i])) == -1) {
+          return 1;
+        }
+      } else {
+        fprintf(stderr, "Unknown flag: %s", argv[i]);
+        return 2;
       }
       continue;
     }
@@ -126,64 +166,95 @@ int processArgs(int argc, char *argv[]) {
   return 0;
 }
 
-// TODO Mostlikely doesn't even work.
+/*
+** This function usually works!
+** Searches the input string for repeating substrings.
+*arguments.
+** @param str the string to be tested for substrings.
+** @return 1 if the message contains substrings, 0 if it doesn't.
+*/
 int countRepSubstrings(char *str, int length, int size) {
-  int repetitions = 0;
 
-  // This loops moves the whole testing offset.
+  // This loops moves the substring offset.
   for (int i = 0; i < length - size; i++) {
-    bool identical = true;
 
     // This one will be iterating through the str
-    for (int j = 0; j < length; j++) {
+    for (int j = 0; j < length - size; j++) {
+      int identical = 0;
       // And this one will be comparing the individual substrings to the rest of
       // the strings.
       for (int k = 0; k < size; k++) {
-        if (i + k == j) {
+        if (i == j) {
           continue;
         }
 
-        if (str[i + k] != str[j]) {
-          fprintf(stderr, "\n%d + %d = %d, %d\n", i, k, i + k, j);
-          identical = false;
-          goto bruh;
+        // fprintf(stdout, "%d + %d = %d; %d + %d = %d\n", i, k, i + k, j, k,
+        //        j + k);
+        if (str[i + k] == str[j + k]) {
+          identical++;
         }
       }
+      if (identical >= size) {
+        return true;
+      }
     }
-
-    if (identical) {
-      repetitions++;
-    }
-  bruh:;
   }
 
-  fprintf(stderr, "Substrings %d\n", repetitions);
-  return repetitions;
+  return false;
 }
 
-int processPassword(char *pass, int level, int param, bool *uniqueChars,
-                    int *shortest, int *averageSum, int *averageCount) {
+/*
+** Chechks if the password passes the requirements.
+** @param pass The password to be tested.
+** @param level The level of password reuqirements
+** @param param A suplemental parameter
+** @param uniqueChars A pointer to an array tracking printable ascii codes that
+*were seen before.
+** @param shortest A pointer to var holidng the length of the shortest passowrd
+*seen yet.
+** @param averageSum The sum of all password lengths.
+** @param averageCount The amount of passwords added to the averageSum variable.
+** @return returns true if the password passed the requirements and false if it
+*did not.
+*/
+bool processPassword(char *pass, int level, int param, bool *uniqueChars,
+                     int *shortest, int *averageSum, int *averageCount) {
   int length = 0;
   int upper = 0;
   int lower = 0;
-  int number = 0;
-  int special = 0;
   int repSequence = 0;
 
   int repetition = 0;
   char lastChar = 0;
+
+  bool hasUpper, hasLower, hasNum, hasSpecial = false;
+  int charGroupCount = 0;
 
   // Analysing the password.
   for (int i = 0; pass[i] != '\0' && pass[i] != '\n'; i++) {
     uniqueChars[((int)pass[i]) - MIN_ALLOWED_CHAR] = true;
     if (pass[i] >= 'a' && pass[i] <= 'z') {
       lower++;
+      if (!hasLower) {
+        hasLower = true;
+        charGroupCount++;
+      }
     } else if (pass[i] >= 'A' && pass[i] <= 'Z') {
       upper++;
+      if (!hasUpper) {
+        hasUpper = true;
+        charGroupCount++;
+      }
     } else if (pass[i] >= '0' && pass[i] <= '9') {
-      number++;
+      if (!hasNum) {
+        hasNum = true;
+        charGroupCount++;
+      }
     } else if (pass[i] >= ' ' && pass[i] <= '~') {
-      special++;
+      if (!hasSpecial) {
+        hasSpecial = true;
+        charGroupCount++;
+      }
     }
 
     // Processing Character Sequences
@@ -218,23 +289,30 @@ int processPassword(char *pass, int level, int param, bool *uniqueChars,
           length, upper, lower, number, special, repSequence);
 #endif
 
-  if (level >= 1 && (upper < 0 || lower < 0)) {
-    return 0;
+  if (level >= 1 && (upper < 1 || lower < 1)) {
+    return false;
   }
-  if (level >= 2 &&
-      (upper < param || lower < param || number < param || special < param)) {
-    return 0;
+  if (level >= 2 && charGroupCount < param) {
+    return false;
   }
-  if (level >= 3 && repSequence != 0) {
-    return 0;
+  if (level >= 3 && (repSequence != 0 || param == 1)) {
+    return false;
   }
   if (level == 4 && countRepSubstrings(pass, length, param)) {
-    return 0;
+    return false;
   }
 
-  return 1;
+  return true;
 }
 
+/*
+** Prints the stats summary to stdout.
+** @param uniques an array of bools parallel to the ASCII values of printable
+*characters seen before in passwords.
+** @param shortest the shortest password seen.
+** @param averageSum The sum of all password lengths.
+** @param averageCount The amount of passwords added to the averageSum variable.
+*/
 void printStats(bool uniques[], int shortest, int avgSum, int avgCount) {
   int uniqueCount = 0;
   for (int i = 0; i < UNIQUE_CHAR_ARRAY_LENGTH; i++) {
@@ -243,8 +321,9 @@ void printStats(bool uniques[], int shortest, int avgSum, int avgCount) {
     }
   }
 
-  fprintf(stdout, "\nStatistics:\n");
-  fprintf(stdout, "Unique Symbols: %d\nShortest: %d\nAverage: %.2f\n",
+  fprintf(stdout, "Statistika:\n");
+  fprintf(stdout,
+          "Ruznych znaku: %d\nMinimalni delka: %d\nPrumerna delka: %.1f\n",
           uniqueCount, shortest, ((float)avgSum) / avgCount);
 }
 
